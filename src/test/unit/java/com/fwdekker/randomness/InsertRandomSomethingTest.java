46daa1cff9a344976dc7eb7ef4dbf4b543c218f8
Felix Dekker <felix@fwdekker.com>
2017-07-18 16:32:19 +0200

    Test edge case and add test documentation

--- src/test/unit/java/com/fwdekker/randomness/InsertRandomSomethingTest.java
+++ src/test/unit/java/com/fwdekker/randomness/InsertRandomSomethingTest.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 
@@ -31,10 +34,28 @@ public final class InsertRandomSomethingTest {
     }
 
 
+    /**
+     * Tests that no further actions are taken when the editor is {@code null}.
+     */
     @Test
-    public void testDisabled() {
+    public void testActionPerformedNullEditor() {
+        final AnActionEvent event = mock(AnActionEvent.class);
+        when(event.getData(CommonDataKeys.EDITOR)).thenReturn(null);
+
+        insertRandomSomething.actionPerformed(event);
+
+        verify(event, times(1)).getData(CommonDataKeys.EDITOR);
+        verifyNoMoreInteractions(event);
+    }
+
+    /**
+     * Tests that the action's presentation is disabled when the editor is null.
+     */
+    @Test
+    public void testUpdateDisabled() {
         final AnActionEvent event = mock(AnActionEvent.class);
         final Presentation presentation = spy(Presentation.class);
+        when(event.getData(CommonDataKeys.EDITOR)).thenReturn(null);
         when(event.getPresentation()).thenReturn(presentation);
 
         insertRandomSomething.update(event);
@@ -42,8 +63,11 @@ public final class InsertRandomSomethingTest {
         assertThat(presentation.isEnabled()).isFalse();
     }
 
+    /**
+     * Tests that the action's presentation is enabled when the editor is not null.
+     */
     @Test
-    public void testEnabled() {
+    public void testUpdateEnabled() {
         final AnActionEvent event = mock(AnActionEvent.class);
         final Presentation presentation = spy(Presentation.class);
         when(event.getData(CommonDataKeys.EDITOR)).thenReturn(mock(Editor.class));
