42d6a735c3372f422368d57a01661f1ee10cb7bb
Felix Dekker <felix@fwdekker.com>
2017-07-18 20:17:41 +0200

    Fix unchecked calls

--- src/test/unit/java/com/fwdekker/randomness/NamedItemTest.java
+++ src/test/unit/java/com/fwdekker/randomness/NamedItemTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class NamedItemTest {
     @Test
     public void testName() {
-        final NamedItem namedItem = new NamedItem("OI6d7AkKye", new Object());
+        final NamedItem<Object> namedItem = new NamedItem<>("OI6d7AkKye", new Object());
 
         assertThat(namedItem.getName()).isEqualTo("OI6d7AkKye");
     }
@@ -22,14 +22,14 @@ public final class NamedItemTest {
     public void testItem() {
         final Object item = new Object();
 
-        final NamedItem namedItem = new NamedItem("mrOz7tVmoX", item);
+        final NamedItem<Object> namedItem = new NamedItem<>("mrOz7tVmoX", item);
 
         assertThat(namedItem.getItem()).isEqualTo(item);
     }
 
     @Test
     public void testToString() {
-        final NamedItem namedItem = new NamedItem("2Z6oCfzfli", new Object());
+        final NamedItem<Object> namedItem = new NamedItem<>("2Z6oCfzfli", new Object());
 
         assertThat(namedItem.toString()).isEqualTo("2Z6oCfzfli");
     }
@@ -37,21 +37,22 @@ public final class NamedItemTest {
 
     @Test
     public void testEqualsSelf() {
-        final NamedItem namedItem = new NamedItem("B4md9THj3y", new Object());
+        final NamedItem<Object> namedItem = new NamedItem<>("B4md9THj3y", new Object());
 
         assertThat(namedItem.equals(namedItem)).isTrue();
     }
 
     @Test
+    @SuppressWarnings("PMD.EqualsNull") // Exactly what is being tested here
     public void testEqualsNull() {
-        final NamedItem namedItem = new NamedItem("c5pgKxqS41", new Object());
+        final NamedItem<Object> namedItem = new NamedItem<>("c5pgKxqS41", new Object());
 
         assertThat(namedItem.equals(null)).isFalse();
     }
 
     @Test
     public void testEqualsOtherClass() {
-        final NamedItem namedItem = new NamedItem("lGdlNMLoyt", new Object());
+        final NamedItem<Object> namedItem = new NamedItem<>("lGdlNMLoyt", new Object());
 
         assertThat(namedItem.equals(new Object())).isFalse();
     }
@@ -61,8 +62,8 @@ public final class NamedItemTest {
         final ArrayList realItem = new ArrayList();
         final RandomAccess itemA = realItem;
         final Cloneable itemB = realItem;
-        final NamedItem namedItemA = new NamedItem("znKHUZqExm", itemA);
-        final NamedItem namedItemB = new NamedItem("znKHUZqExm", itemB);
+        final NamedItem<RandomAccess> namedItemA = new NamedItem<>("znKHUZqExm", itemA);
+        final NamedItem<Cloneable> namedItemB = new NamedItem<>("znKHUZqExm", itemB);
 
         assertThat(namedItemA.equals(namedItemB)).isTrue();
     }
@@ -70,16 +71,16 @@ public final class NamedItemTest {
     @Test
     public void testEqualsOtherName() {
         final Object item = new Object();
-        final NamedItem namedItemA = new NamedItem("osEjuXELqh", item);
-        final NamedItem namedItemB = new NamedItem("BOGVSgfrjF", item);
+        final NamedItem<Object> namedItemA = new NamedItem<>("osEjuXELqh", item);
+        final NamedItem<Object> namedItemB = new NamedItem<>("BOGVSgfrjF", item);
 
         assertThat(namedItemA.equals(namedItemB)).isFalse();
     }
 
     @Test
     public void testEqualsOtherItem() {
-        final NamedItem namedItemA = new NamedItem("iMmu1w7Doi", new Object());
-        final NamedItem namedItemB = new NamedItem("iMmu1w7Doi", new Object());
+        final NamedItem<Object> namedItemA = new NamedItem<>("iMmu1w7Doi", new Object());
+        final NamedItem<Object> namedItemB = new NamedItem<>("iMmu1w7Doi", new Object());
 
         assertThat(namedItemA.equals(namedItemB)).isFalse();
     }
@@ -87,8 +88,8 @@ public final class NamedItemTest {
     @Test
     public void testEqualsSame() {
         final Object item = new Object();
-        final NamedItem namedItemA = new NamedItem("zIIRUo8lPz", item);
-        final NamedItem namedItemB = new NamedItem("zIIRUo8lPz", item);
+        final NamedItem<Object> namedItemA = new NamedItem<>("zIIRUo8lPz", item);
+        final NamedItem<Object> namedItemB = new NamedItem<>("zIIRUo8lPz", item);
 
         assertThat(namedItemA.equals(namedItemB)).isTrue();
     }
@@ -96,7 +97,7 @@ public final class NamedItemTest {
 
     @Test
     public void testHashCodeSelf() {
-        final NamedItem namedItem = new NamedItem("2UCKZ9kZ7o", new Object());
+        final NamedItem<Object> namedItem = new NamedItem<>("2UCKZ9kZ7o", new Object());
 
         assertThat(namedItem.hashCode()).isEqualTo(namedItem.hashCode());
     }
@@ -106,8 +107,8 @@ public final class NamedItemTest {
         final ArrayList realItem = new ArrayList();
         final RandomAccess itemA = realItem;
         final Cloneable itemB = realItem;
-        final NamedItem namedItemA = new NamedItem("vGi5P7o96T", itemA);
-        final NamedItem namedItemB = new NamedItem("vGi5P7o96T", itemB);
+        final NamedItem<RandomAccess> namedItemA = new NamedItem<>("vGi5P7o96T", itemA);
+        final NamedItem<Cloneable> namedItemB = new NamedItem<>("vGi5P7o96T", itemB);
 
         assertThat(namedItemA.hashCode()).isEqualTo(namedItemB.hashCode());
     }
@@ -115,16 +116,16 @@ public final class NamedItemTest {
     @Test
     public void testHashCodeOtherName() {
         final Object item = new Object();
-        final NamedItem namedItemA = new NamedItem("DpMSRx40TF", item);
-        final NamedItem namedItemB = new NamedItem("jcnHjZQlOq", item);
+        final NamedItem<Object> namedItemA = new NamedItem<>("DpMSRx40TF", item);
+        final NamedItem<Object> namedItemB = new NamedItem<>("jcnHjZQlOq", item);
 
         assertThat(namedItemA.hashCode()).isNotEqualTo(namedItemB.hashCode());
     }
 
     @Test
     public void testHashCodeOtherItem() {
-        final NamedItem namedItemA = new NamedItem("iMmu1w7Doi", new Object());
-        final NamedItem namedItemB = new NamedItem("iMmu1w7Doi", new Object());
+        final NamedItem<Object> namedItemA = new NamedItem<>("iMmu1w7Doi", new Object());
+        final NamedItem<Object> namedItemB = new NamedItem<>("iMmu1w7Doi", new Object());
 
         assertThat(namedItemA.hashCode()).isNotEqualTo(namedItemB.hashCode());
     }
@@ -132,8 +133,8 @@ public final class NamedItemTest {
     @Test
     public void testHashCodeSame() {
         final Object item = new Object();
-        final NamedItem namedItemA = new NamedItem("hglHcOsYu0", item);
-        final NamedItem namedItemB = new NamedItem("hglHcOsYu0", item);
+        final NamedItem<Object> namedItemA = new NamedItem<>("hglHcOsYu0", item);
+        final NamedItem<Object> namedItemB = new NamedItem<>("hglHcOsYu0", item);
 
         assertThat(namedItemA.hashCode()).isEqualTo(namedItemB.hashCode());
     }
