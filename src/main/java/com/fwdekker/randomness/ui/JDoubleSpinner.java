dd1cf791015ed281722c1670c8a80ba0d19ef3fa
Felix Dekker <felix@fwdekker.com>
2017-07-25 22:37:28 +0200

    Add specialised spinner classes

---
+++ src/main/java/com/fwdekker/randomness/ui/JDoubleSpinner.java
@@ -0,0 +1,96 @@
+package com.fwdekker.randomness.ui;
+
+import com.fwdekker.randomness.common.ValidationException;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+
+/**
+ * A {@link JSpinner} for doubles.
+ * <p>
+ * A {@code JDoubleSpinner} can only represent value from {@value DEFAULT_MIN_VALUE} (inclusive) until {@value
+ * DEFAULT_MAX_VALUE} (inclusive), because not all numbers outside this range can be represented as a {@link Double}.
+ */
+public final class JDoubleSpinner extends JSpinner {
+    /**
+     * The default step size when decrementing or incrementing the value.
+     */
+    private static final double DEFAULT_STEP_SIZE = 0.1;
+    /**
+     * The smallest number that can be represented by a {@code JDoubleSpinner}.
+     */
+    private static final double DEFAULT_MIN_VALUE = -1E53;
+    /**
+     * The largest number that may be represented by a {@code JDoubleSpinner}.
+     */
+    private static final double DEFAULT_MAX_VALUE = 1E53;
+
+    /**
+     * The smallest number that may be represented by this {@code JDoubleSpinner}.
+     */
+    private final double minValue;
+    /**
+     * The largest number that may be represented by this {@code JDoubleSpinner}.
+     */
+    private final double maxValue;
+
+
+    /**
+     * Constructs a new {@code JDoubleSpinner}.
+     */
+    public JDoubleSpinner() {
+        super(new SpinnerNumberModel(0.0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, DEFAULT_STEP_SIZE));
+
+        this.minValue = DEFAULT_MIN_VALUE;
+        this.maxValue = DEFAULT_MAX_VALUE;
+
+        final JSpinner.NumberEditor editor = new JSpinner.NumberEditor(this);
+        editor.getFormat().setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
+        setEditor(editor);
+    }
+
+    /**
+     * Constructs a new {@code JDoubleSpinner}.
+     *
+     * @param minValue the smallest number that may be represented by this {@code JDoubleSpinner}
+     * @param maxValue the largest number that may be represented by this {@code JDoubleSpinner}
+     */
+    public JDoubleSpinner(final double minValue, final double maxValue) {
+        if (minValue < DEFAULT_MIN_VALUE) {
+            throw new IllegalArgumentException("minValue should not be smaller than " + DEFAULT_MIN_VALUE + ".");
+        }
+        if (maxValue > DEFAULT_MAX_VALUE) {
+            throw new IllegalArgumentException("maxValue should not be greater than " + DEFAULT_MAX_VALUE + ".");
+        }
+        if (minValue > maxValue) {
+            throw new IllegalArgumentException("minValue should be greater than maxValue.");
+        }
+
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+
+    @Override
+    public Double getValue() {
+        return ((Number) super.getValue()).doubleValue();
+    }
+
+    /**
+     * Validates the current value and throws an exception if the value is invalid.
+     *
+     * @throws ValidationException if the value is not valid
+     */
+    public void validateValue() throws ValidationException {
+        final double value = getValue();
+
+        if (value < minValue) {
+            throw new ValidationException("Please enter a value greater than or equal to " + minValue + ".", this);
+        }
+        if (value > maxValue) {
+            throw new ValidationException("Please enter a value less than or equal to " + maxValue + ".", this);
+        }
+    }
+}
---
+++ src/test/unit/java/com/fwdekker/randomness/ui/JDoubleSpinnerTest.java
@@ -0,0 +1,98 @@
+package com.fwdekker.randomness.ui;
+
+import com.fwdekker.randomness.common.ValidationException;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+/**
+ * Unit tests for {@link JDoubleSpinner}.
+ */
+public final class JDoubleSpinnerTest {
+    @Test
+    public void testIllegalMinValue() {
+        assertThatThrownBy(() -> new JDoubleSpinner(-1E80, -477.23))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("minValue should not be smaller than -1.0E53.");
+    }
+
+    @Test
+    public void testIllegalMaxValue() {
+        assertThatThrownBy(() -> new JDoubleSpinner(-161.29, 1E73))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("maxValue should not be greater than 1.0E53.");
+    }
+
+    @Test
+    public void testIllegalRange() {
+        assertThatThrownBy(() -> new JDoubleSpinner(-602.98, -929.41))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("minValue should be greater than maxValue.");
+    }
+
+
+    @Test
+    public void testGetSetValue() {
+        final JDoubleSpinner spinner = new JDoubleSpinner();
+
+        spinner.setValue(179.40);
+
+        assertThat(spinner.getValue()).isEqualTo(179.40);
+    }
+
+    @Test
+    public void testGetSetValueType() {
+        final JDoubleSpinner spinner = new JDoubleSpinner();
+
+        spinner.setValue(638L);
+
+        assertThat(spinner.getValue()).isEqualTo(638.0);
+    }
+
+
+    @Test
+    public void testValidateUnderflow() {
+        final JDoubleSpinner spinner = new JDoubleSpinner();
+
+        spinner.setValue(-1E55);
+
+        assertThatThrownBy(() -> spinner.validateValue())
+                .isInstanceOf(ValidationException.class)
+                .hasMessage("Please enter a value greater than or equal to -1.0E53.");
+    }
+
+    @Test
+    public void testValidateOverflow() {
+        final JDoubleSpinner spinner = new JDoubleSpinner();
+
+        spinner.setValue(1E98);
+
+        assertThatThrownBy(() -> spinner.validateValue())
+                .isInstanceOf(ValidationException.class)
+                .hasMessage("Please enter a value less than or equal to 1.0E53.");
+    }
+
+    @Test
+    public void testValidateUnderflowCustomRange() {
+        final JDoubleSpinner spinner = new JDoubleSpinner(-738.33, 719.45);
+
+        spinner.setValue(-808.68);
+
+        assertThatThrownBy(() -> spinner.validateValue())
+                .isInstanceOf(ValidationException.class)
+                .hasMessage("Please enter a value greater than or equal to -738.33.");
+    }
+
+    @Test
+    public void testValidateOverflowCustomRange() {
+        final JDoubleSpinner spinner = new JDoubleSpinner(-972.80, -69.36);
+
+        spinner.setValue(94.0);
+
+        assertThatThrownBy(() -> spinner.validateValue())
+                .isInstanceOf(ValidationException.class)
+                .hasMessage("Please enter a value less than or equal to -69.36.");
+    }
+}
