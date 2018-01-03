1220485dc611401b72c9cf08f29f2f86f4e7aadb
Felix Dekker <felix@fwdekker.com>
2018-01-03 12:22:12 +0100

    Update tests

--- src/test/unit/java/com/fwdekker/randomness/word/DictionaryTest.java
+++ src/test/unit/java/com/fwdekker/randomness/word/DictionaryTest.java
@@ -3,40 +3,105 @@ package com.fwdekker.randomness.word;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 /**
  * Unit tests for {@link Dictionary}.
  */
 public final class DictionaryTest {
+    private Dictionary dictionary;
+
+
+    @Test
+    public void testEmptyDictionary() {
+        assertThatThrownBy(() -> new Dictionary("dictionaries/empty.dic"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Dictionary must be non-empty.")
+                .hasNoCause();
+    }
+
+
     @Test
     public void testGetWordsWithLengthInRangeInvertedRange() {
-        assertThat(Dictionary.getWordsWithLengthInRange(1000, 0)).isEmpty();
+        useDictionary("simple");
+
+        assertThat(dictionary.getWordsWithLengthInRange(696, 54))
+                .isEmpty();
     }
 
     @Test
     public void testGetWordsWithLengthInRangeNegativeLength() {
-        assertThat(Dictionary.getWordsWithLengthInRange(-1000, 0)).isEmpty();
+        useDictionary("simple");
+
+        assertThat(dictionary.getWordsWithLengthInRange(-4, -2))
+                .isEmpty();
     }
 
     @Test
-    public void testGetWordsWithLengthInRangeEmptyWord() {
-        assertThat(Dictionary.getWordsWithLengthInRange(0, 0)).isEmpty();
+    public void testGetWordsWithLengthInRangeEmptyRange() {
+        useDictionary("simple");
+
+        assertThat(dictionary.getWordsWithLengthInRange(0, 0))
+                .isEmpty();
+    }
+
+    @Test
+    public void testGetWordsWithLengthInRangeOvershortWord() {
+        useDictionary("varied");
+
+        assertThat(dictionary.getWordsWithLengthInRange(0, 1))
+                .isEmpty();
     }
 
     @Test
     public void testGetWordsWithLengthInRangeShortWord() {
-        assertThat(Dictionary.getWordsWithLengthInRange(0, 1)).isNotEmpty();
+        useDictionary("simple");
+
+        assertThat(dictionary.getWordsWithLengthInRange(1, 1))
+                .containsExactly("a");
     }
 
     @Test
     public void testGetWordsWithLengthInRangeLongWord() {
-        assertThat(Dictionary.getWordsWithLengthInRange(1000, 1001)).isEmpty();
+        useDictionary("varied");
+
+        assertThat(dictionary.getWordsWithLengthInRange(40, 50))
+                .containsExactly("pneumonoultramicroscopicsilicovolcanoconiosis");
+    }
+
+    @Test
+    public void testGetWordsWithLengthInRangeOverlongWord() {
+        useDictionary("simple");
+
+        assertThat(dictionary.getWordsWithLengthInRange(1000, 1001))
+                .isEmpty();
     }
 
 
     @Test
-    public void testLongestWordLength() {
-        assertThat(Dictionary.longestWordLength()).isEqualTo(31);
+    public void testLongestWordSimpleLength() {
+        useDictionary("simple");
+
+        assertThat(dictionary.longestWordLength())
+                .isEqualTo(4);
+    }
+
+    @Test
+    public void testLongestWordVariedLength() {
+        useDictionary("varied");
+
+        assertThat(dictionary.longestWordLength())
+                .isEqualTo(45);
+    }
+
+
+    /**
+     * Selects the {@link Dictionary} to use for the current test.
+     *
+     * @param dictionaryName the filename of the dictionary to use for the current test
+     */
+    private void useDictionary(final String dictionaryName) {
+        dictionary = new Dictionary("dictionaries/" + dictionaryName + ".dic");
     }
 }
