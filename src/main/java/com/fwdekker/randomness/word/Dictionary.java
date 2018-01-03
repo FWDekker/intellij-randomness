238fd0b18927d1e44e1365cf2dae6d668b694510
Felix Dekker <felix@fwdekker.com>
2018-01-03 11:07:53 +0100

    Treat .dictionary files as binary

--- src/main/java/com/fwdekker/randomness/word/Dictionary.java
+++ src/main/java/com/fwdekker/randomness/word/Dictionary.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,7 +16,7 @@ public final class Dictionary {
     /**
      * The name of the dictionary file.
      */
-    public static final String DICTIONARY_FILE = "words_alpha.txt";
+    public static final String DICTIONARY_FILE = "words_alpha.dictionary";
 
     /**
      * A list of all words in the dictionary.
