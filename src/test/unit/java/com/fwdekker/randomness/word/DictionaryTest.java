a115b6f6fa3961f9438a9f6be62922d8f1aa5faa
Felix Dekker <felix@fwdekker.com>
2018-01-03 03:58:10 +0100

    Fix import

--- src/test/unit/java/com/fwdekker/randomness/word/DictionaryTest.java
+++ src/test/unit/java/com/fwdekker/randomness/word/DictionaryTest.java
@@ -2,7 +2,7 @@ package com.fwdekker.randomness.word;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
