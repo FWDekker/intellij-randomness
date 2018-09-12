9f4138ce39f34208c90147485a6f12e2872492b0
Felix Dekker <felix@fwdekker.com>
2018-09-12 10:21:10 +0200

    Add stub UUID settings window

---
+++ src/main/java/com/fwdekker/randomness/uuid/UuidSettings.java
@@ -0,0 +1,38 @@
+package com.fwdekker.randomness.uuid;
+
+import com.fwdekker.randomness.Settings;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+
+
+/**
+ * Contains settings for generating random UUIDs.
+ */
+@State(
+        name = "IntegerSettings",
+        storages = @Storage("$APP_CONFIG$/randomness.xml")
+)
+public final class UuidSettings extends Settings implements PersistentStateComponent<UuidSettings> {
+    /**
+     * Returns the singleton {@code UuidSettings} instance.
+     *
+     * @return the singleton {@code UuidSettings} instance
+     */
+    public static UuidSettings getInstance() {
+        return ServiceManager.getService(UuidSettings.class);
+    }
+
+    @Override
+    public UuidSettings getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(final @NotNull UuidSettings state) {
+        XmlSerializerUtil.copyBean(state, this);
+    }
+}
---
+++ src/main/java/com/fwdekker/randomness/uuid/UuidSettingsDialog.java
@@ -0,0 +1,59 @@
+package com.fwdekker.randomness.uuid;
+
+import com.fwdekker.randomness.SettingsDialog;
+import com.intellij.openapi.ui.ValidationInfo;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+
+/**
+ * Dialog for settings of random UUID generation.
+ */
+public class UuidSettingsDialog extends SettingsDialog<UuidSettings> {
+    private JPanel contentPane;
+
+
+    /**
+     * Constructs a new {@code UuidSettingsDialog} that uses the singleton {@code UuidSettings} instance.
+     */
+    UuidSettingsDialog() {
+        this(UuidSettings.getInstance());
+    }
+
+    /**
+     * Constructs a new {@code UuidSettingsDialog} that uses the given {@code UuidSettings} instance.
+     *
+     * @param settings the settings to manipulate with this dialog
+     */
+    UuidSettingsDialog(final @NotNull UuidSettings settings) {
+        super(settings);
+
+        init();
+        loadSettings();
+    }
+
+
+    @Override
+    @NotNull
+    protected JComponent createCenterPanel() {
+        return contentPane;
+    }
+
+    @Override
+    @Nullable
+    protected ValidationInfo doValidate() {
+        return null;
+    }
+
+
+    @Override
+    public void loadSettings(final @NotNull UuidSettings settings) {
+    }
+
+    @Override
+    public void saveSettings(final @NotNull UuidSettings settings) {
+    }
+}
