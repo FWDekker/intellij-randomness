c73695ad59e32ea378ebe7240271d56bc5a21ad7
F.W. Dekker <f@fwdekker.com>
2022-01-03 16:55:01 +0100

    Add button to open supported regex syntax

--- src/main/kotlin/com/fwdekker/randomness/string/StringSchemeEditor.kt
+++ src/main/kotlin/com/fwdekker/randomness/string/StringSchemeEditor.kt
@@ -11,9 +11,11 @@ import com.fwdekker.randomness.ui.setLabel
 import com.fwdekker.randomness.ui.setValue
 import com.intellij.ui.SeparatorFactory
 import com.intellij.ui.TitledSeparator
+import com.intellij.ui.components.BrowserLink
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.util.ui.UI
 import javax.swing.ButtonGroup
+import javax.swing.JButton
 import javax.swing.JCheckBox
 import javax.swing.JLabel
 import javax.swing.JPanel
@@ -33,6 +35,7 @@ class StringSchemeEditor(scheme: StringScheme = StringScheme()) : StateEditor<St
 
     private lateinit var valueSeparator: TitledSeparator
     private lateinit var patternField: JTextField
+    private lateinit var patternHelpButton: JButton
     private lateinit var isRegexCheckBox: JCheckBox
     private lateinit var capitalizationLabel: JLabel
     private lateinit var capitalizationGroup: ButtonGroup
@@ -58,6 +61,11 @@ class StringSchemeEditor(scheme: StringScheme = StringScheme()) : StateEditor<St
     @Suppress("UnusedPrivateMember") // Used by scene builder
     private fun createUIComponents() {
         valueSeparator = SeparatorFactory.createSeparator(Bundle("string.ui.value_separator"), null)
+
+        patternHelpButton = BrowserLink(
+            Bundle("string.ui.pattern_help"),
+            "https://github.com/curious-odd-man/RgxGen/tree/1.3#supported-syntax"
+        )
 
         removeLookAlikeSymbolsCheckBox = JBCheckBox(Bundle("string.ui.remove_look_alike"))
             .also { box ->
