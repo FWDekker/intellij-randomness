de252e0cce4d24e2de8e91de3dd0ae91a34f15ef
F.W. Dekker <f@fwdekker.com>
2022-01-03 15:07:34 +0100

    Resolve minor misuse of minCount in test

--- src/test/kotlin/com/fwdekker/randomness/SchemeTest.kt
+++ src/test/kotlin/com/fwdekker/randomness/SchemeTest.kt
@@ -93,12 +93,12 @@ object SchemeTest : Spek({
 
         it("applies decorators on each other in ascending order") {
             scheme.decorators = listOf(
-                ArrayDecorator(enabled = true, minCount = 2),
-                ArrayDecorator(enabled = true, minCount = 3),
+                ArrayDecorator(enabled = true, minCount = 2, maxCount = 2),
+                ArrayDecorator(enabled = true, minCount = 3, maxCount = 3),
             )
             scheme.literals = listOf("save")
 
-            assertThat(scheme.generateStrings(1)).containsExactly("[[save, save], [save, save], [save, save]]")
+            assertThat(scheme.generateStrings()).containsExactly("[[save, save], [save, save], [save, save]]")
         }
     }
 })
