FWDekker/intellij-randomness

commit 13af6e6a3cd599f4d181dd84eaee38fbea54fb2e

Implement an assortment of various fixes
See #526 for more details.

src/main/kotlin/com/fwdekker/randomness/InsertAction.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.newEditor.SettingsDialogFactory
+import com.intellij.util.alsoIfNull
 import javax.swing.Icon
 
 
@@ -66,8 +67,10 @@ abstract class InsertAction(
         val project = event.getData(CommonDataKeys.PROJECT) ?: return
 
         configurable?.also {
-            if (!SettingsDialogFactory.getInstance().create(project, text, it, false, false).showAndGet())
-                return
+            SettingsDialogFactory.getInstance()
+                .create(project, text, it, false, false)
+                .showAndGet()
+                .alsoIfNull { return }
         }
 
         val data =
@@ -89,7 +92,7 @@ abstract class InsertAction(
                 return
             }
 
-        WriteCommandAction.runWriteCommandAction(project) {
+        WriteCommandAction.runWriteCommandAction(project, Bundle("misc.insert_command_name"), null, {
             editor.caretModel.allCarets.forEachIndexed { i, caret ->
                 val start = caret.selectionStart
                 val end = caret.selectionEnd
@@ -98,7 +101,7 @@ abstract class InsertAction(
                 editor.document.replaceString(start, end, data[i])
                 caret.setSelection(start, newEnd)
             }
-        }
+        })
     }
 
 

build.gradle.kts
@@ -17,24 +17,24 @@ fun properties(key: String) = project.findProperty(key).toString()
 /// Plugins
 plugins {
     // Compilation
-    id("org.jetbrains.kotlin.jvm") version "1.9.21"  // Use latest version, ignoring `gradle.properties`
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"  // Set to latest version compatible with `pluginSinceBuild`, cf. https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library
     id("org.jetbrains.intellij") version "1.16.1"
 
     // Tests/coverage
     id("org.jetbrains.kotlinx.kover") version "0.7.5"
 
     // Static analysis
-    id("io.gitlab.arturbosch.detekt") version "1.23.4"  // See also `gradle.properties`
+    id("io.gitlab.arturbosch.detekt") version "1.23.4"  // cf. `gradle.properties`
 
     // Documentation
     id("org.jetbrains.changelog") version "2.2.0"
-    id("org.jetbrains.dokka") version "1.9.10"  // See also `buildscript { dependencies` below and `gradle.properties`
+    id("org.jetbrains.dokka") version "1.9.10"  // cf. `buildscript { dependencies` below and `gradle.properties`
 }
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.dokka:dokka-base:1.9.10")  // See also `plugins` above and `gradle.properties`
-        classpath("org.jetbrains.dokka:versioning-plugin:1.9.10")  // See also `plugins` above and `gradle.properties`
+        classpath("org.jetbrains.dokka", "dokka-base", "1.9.10")  // cf. `plugins` above and `gradle.properties`
+        classpath("org.jetbrains.dokka", "versioning-plugin", "1.9.10")  // cf. `plugins` above and `gradle.properties`
     }
 }
 
@@ -45,22 +45,22 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.uuid:java-uuid-generator:${properties("uuidGeneratorVersion")}")
-    implementation("com.github.sisyphsu:dateparser:${properties("dateparserVersion")}")
-    implementation("com.github.curious-odd-man:rgxgen:${properties("rgxgenVersion")}")
-    implementation("com.vdurmont:emoji-java:${properties("emojiVersion")}")
-    implementation("org.eclipse.mylyn.github:org.eclipse.egit.github.core:${properties("githubCore")}")
-    api("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.uuid", "java-uuid-generator", properties("uuidGeneratorVersion"))
+    implementation("com.github.sisyphsu", "dateparser", properties("dateparserVersion")) {
+        exclude(group = "org.projectlombok", module = "lombok")  // cf. https://github.com/sisyphsu/dateparser/issues/30
+    }
+    implementation("com.github.curious-odd-man", "rgxgen", properties("rgxgenVersion"))
+    implementation("org.eclipse.mylyn.github", "org.eclipse.egit.github.core", properties("githubCore"))
 
-    testImplementation("org.assertj:assertj-swing-junit:${properties("assertjSwingVersion")}")
-    testRuntimeOnly("org.junit.platform:junit-platform-runner:${properties("junitRunnerVersion")}")
-    testImplementation("org.junit.vintage:junit-vintage-engine:${properties("junitVersion")}")
-    testImplementation("io.kotest:kotest-assertions-core:${properties("kotestVersion")}")
-    testImplementation("io.kotest:kotest-framework-datatest:${properties("kotestVersion")}")
-    testImplementation("io.kotest:kotest-runner-junit5:${properties("kotestVersion")}")
+    testImplementation("org.assertj", "assertj-swing-junit", properties("assertjSwingVersion"))
+    testRuntimeOnly("org.junit.platform", "junit-platform-runner", properties("junitRunnerVersion"))
+    testImplementation("org.junit.vintage", "junit-vintage-engine", properties("junitVersion"))
+    testImplementation("io.kotest", "kotest-assertions-core", properties("kotestVersion"))
+    testImplementation("io.kotest", "kotest-framework-datatest", properties("kotestVersion"))
+    testImplementation("io.kotest", "kotest-runner-junit5", properties("kotestVersion"))
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${properties("detektVersion")}")
-    dokkaHtmlPlugin("org.jetbrains.dokka:versioning-plugin:${properties("dokkaVersion")}")
+    detektPlugins("io.gitlab.arturbosch.detekt", "detekt-formatting", properties("detektVersion"))
+    dokkaHtmlPlugin("org.jetbrains.dokka", "versioning-plugin", properties("dokkaVersion"))
 }
 
 
@@ -74,7 +74,7 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = properties("javaVersion")
-            apiVersion = properties("kotlinApiVersion")
+            apiVersion = properties("kotlinVersion")
             languageVersion = properties("kotlinVersion")
         }
     }

gradle.properties
@@ -20,13 +20,12 @@ pluginVerifierIdeVersions=IC-2023.1.5, IC-2023.2.5, IC-2023.3, CL-2023.1.5, CL-2
 #     Java version should be the one used by the oldest Randomness-supported version of IntelliJ. See also
 #     https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions.
 #
-# * Kotlin
-#   * `kotlinVersion` is the same as `kotlinApiVersion`.
-#   * Kotlin version should be bundled stdlib version of oldest supported IntelliJ version. See also
-#     https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library.
+# * Kotlin:
+#     * Kotlin version should be bundled stdlib version of oldest supported IntelliJ version. See also
+#       https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library.
+#     * Ensure the version of the `org.jetbrains.kotlin.jvm` in `build.gradle.kts` is updated accordingly.
 javaVersion=17
 kotlinVersion=1.8
-kotlinApiVersion=1.8
 
 # Dependencies
 # * Detekt should also be updated in `plugins` block.

src/main/kotlin/com/fwdekker/randomness/ErrorReporter.kt
@@ -43,7 +43,7 @@ import javax.crypto.spec.SecretKeySpec
  * Heavily inspired by [Patrick Scheibe's error reporter](https://github.com/halirutan/Wolfram-Language-IntelliJ-Plugin-Archive/tree/e3dd72f9cd344d678ac892aaa7bf59abd84871e8/src/de/halirutan/mathematica/errorreporting).
  */
 @Suppress("detekt:MaxLineLength") // Necessary because of the long link in the docs above
-class ErrorReporter : ErrorReportSubmitter() {
+internal class ErrorReporter : ErrorReportSubmitter() {
     /**
      * Interacts with GitHub.
      */
@@ -150,7 +150,7 @@ private class GitHubReporter {
     fun report(issueData: IssueData): SubmittedReportInfo =
         try {
             synchronized(this) {
-                val duplicate = issueService.pageIssues(repo).flatSequence().firstOrNull(issueData::isDuplicateOf)
+                val duplicate = issueService.pageIssues(repo).flatSequence().firstOrNull { issueData.isDuplicateOf(it) }
 
                 val context: Issue
                 if (duplicate == null) {
@@ -241,7 +241,7 @@ private object GitHubScrambler {
      * The URL at which a newer token may be available.
      */
     private val URL =
-        URL("http://raw.githubusercontent.com/FWDekker/intellij-randomness/main/src/main/resources/reporter/token.bin")
+        URL("https://raw.githubusercontent.com/FWDekker/intellij-randomness/main/src/main/resources/reporter/token.bin")
 
 
     /**
@@ -340,16 +340,27 @@ private class IssueData(
      */
     val body: String =
         emptySequence<Pair<String, String>>()
-            .plus("User-supplied comments" to additionalInfo.ifNullOrBlank { "_No comments supplied._" }.trim())
+            .plus(
+                Bundle("reporter.issue.body.comments.title") to
+                    additionalInfo.ifNullOrBlank { italic(Bundle("reporter.issue.body.comments.body_empty")) }.trim()
+            )
             .plus(
                 events
                     .map { it.throwableText }
                     .filterNot { it.isBlank() }
-                    .mapIndexed { idx: Int, body: String -> "Stacktrace ${idx + 1}" to spoiler(code(body, "java")) }
+                    .mapIndexed { idx: Int, body: String ->
+                        Bundle("reporter.issue.body.stacktrace.title", idx + 1) to
+                            spoiler(code(body, "java"))
+                    }
             )
-            .plus(attachments.map { "Attachment: `${it.name}`" to spoiler(code(it.displayText)) })
             .plus(
-                "Version information" to
+                attachments.map {
+                    Bundle("reporter.issue.body.attachment.title", it.name) to
+                        spoiler(code(it.displayText))
+                }
+            )
+            .plus(
+                Bundle("reporter.issue.body.version.title") to
                     """
                     - Randomness version: ${pluginDescriptor.version ?: "_Unknown_"}
                     - IDE version: ${ApplicationInfo.getInstance().apiVersion}
@@ -358,7 +369,7 @@ private class IssueData(
                     """.trimIndent()
             )
             .joinToString(separator = "\n\n") { section(it.first, it.second) }
-            .plus("\n\n---\n\n_This issue report was generated automatically for an anonymous user._")
+            .let { italic(Bundle("reporter.issue.body.header")) + "\n\n---\n\n" + it }
 
 
     /**
@@ -383,6 +394,17 @@ private class IssueData(
      */
     companion object {
         /**
+         * Creates a Markdown code block containing [body] and using the given [language].
+         */
+        private fun code(body: String, language: String = ""): String =
+            "```$language\n$body\n```"
+
+        /**
+         * Creates an italicized piece of Markdown.
+         */
+        private fun italic(body: String) = "_${body}_"
+
+        /**
          * Creates a Markdown section with the given [title] and [body].
          */
         private fun section(title: String, body: String): String =
@@ -393,12 +415,6 @@ private class IssueData(
          */
         private fun spoiler(body: String, heading: String = "Click to show"): String =
             "<details>\n  <summary>${heading.trim()}</summary>\n\n${body.prependIndent("  ")}\n\n</details>"
-
-        /**
-         * Creates a Markdown code block containing [body] and using the given [language].
-         */
-        private fun code(body: String, language: String = ""): String =
-            "```$language\n$body\n```"
 
 
         /**

src/main/kotlin/com/fwdekker/randomness/Notifier.kt
@@ -8,18 +8,18 @@ import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.ui.MessageDialogBuilder
 
 
 /**
  * Displays notifications when a project is opened.
  */
-class Notifier : StartupActivity {
+internal class Notifier : ProjectActivity {
     /**
      * Shows startup notifications.
      */
-    override fun runActivity(project: Project) = showWelcomeToV3(project)
+    override suspend fun execute(project: Project) = showWelcomeToV3(project)
 
     /**
      * Shows a notification introducing the user to version 3 of Randomness.

src/main/kotlin/com/fwdekker/randomness/Settings.kt
@@ -77,7 +77,7 @@ data class Settings(
     ],
     category = SettingsCategory.PLUGINS,
 )
-class PersistentSettings : PersistentStateComponent<Element> {
+internal class PersistentSettings : PersistentStateComponent<Element> {
     /**
      * The [Settings] that should be persisted.
      *

src/main/kotlin/com/fwdekker/randomness/Timely.kt
@@ -28,7 +28,7 @@ object Timely {
         try {
             return executor.submit<T> { generator() }.get(GENERATOR_TIMEOUT, TimeUnit.MILLISECONDS)
         } catch (exception: TimeoutException) {
-            throw DataGenerationException(Bundle("helpers.error.timed_out"), exception)
+            throw DataGenerationException(Bundle("misc.timed_out"), exception)
         } catch (exception: ExecutionException) {
             throw DataGenerationException(exception.cause?.message ?: exception.message, exception)
         } finally {

src/main/kotlin/com/fwdekker/randomness/string/StringScheme.kt
@@ -141,7 +141,7 @@ data class StringScheme(
         /**
          * The default value of the [capitalization] field.
          */
-        val DEFAULT_CAPITALIZATION get() = CapitalizationMode.RETAIN
+        val DEFAULT_CAPITALIZATION = CapitalizationMode.RETAIN
 
         /**
          * The default value of the [removeLookAlikeSymbols] field.

commit 230dac3ef33939c577041895956adab61c7d994d

Fix #529
The bug stemmed from an error introduced in 13af6e6a3cd599f4d181dd84eaee38fbea54fb2e.

src/main/kotlin/com/fwdekker/randomness/InsertAction.kt
@@ -9,7 +9,6 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.newEditor.SettingsDialogFactory
-import com.intellij.util.alsoIfNull
 import javax.swing.Icon
 
 
@@ -70,7 +69,7 @@ abstract class InsertAction(
             SettingsDialogFactory.getInstance()
                 .create(project, text, it, false, false)
                 .showAndGet()
-                .alsoIfNull { return }
+                .also { pressedOK -> if (!pressedOK) return }
         }
 
         val data =